34f4d8e4fec9e72bce550444d3fe863649c1c010
yugesh thippana <yugeshkopayusi@gmail.com>
2022-04-26 04:03:14 +0530

    basic snippet layout: done, todo- styling, adding css-snippets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useCallback, useMemo, useState } from 'react';
 import './App.css';
+import ThreeDtText from './Components/ThreeDtText';
 
 function App() {
+  const [currentSelector, setCurrentSelector] = useState(null);
+  const [isCodeOpen, setIsCodeOpen] = useState(false);
+
+  const selectorComponents = useMemo(() => {
+    return [{
+      name: "3dtext",
+      component: <ThreeDtText />, 
+    }]
+  }, [])
+
+  const selectorComponent = useMemo(() => {
+    return selectorComponents.find((c) => c.name === currentSelector)?.component
+  } ,[currentSelector, selectorComponents]) 
+
+  const updateSelector = useCallback((name) => {
+    setCurrentSelector(name);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header"> 
+        <div>
+          Reusable css components
+        </div> 
+      </div>
+
+      <div className="main">
+        {currentSelector ? 
+        <>
+          <div className='back-button' onClick={() => setCurrentSelector(null)}> {"<-"} </div>
+
+          <div className="selector-component">
+            {selectorComponent}
+
+            <div className="selector-actions">
+              <div className="btn btn-primary" onClick={() => setIsCodeOpen(true)}>view source code</div>
+              <div className="btn btn-primary">Download</div>
+            </div>
+
+            <div className="source-code-container accordion-item">
+             
+              {isCodeOpen && 
+                <div style = {{width: "90%", border: "1px solid rgba(0, 0, 0, 0.279)", padding: "30px"}}>
+                  <div className="code-close-btn" >
+                    <div> {""} </div>
+                    <div>{currentSelector} </div>
+                    <div onClick={()=> setIsCodeOpen(false)}>X </div>
+                  </div>
+
+                  <div className="source-code">
+                    yor code goes here
+                  </div>
+                </div>
+            }  
+            </div>
+          </div>
+        </>
+        : 
+          selectorComponents.map((c) => (
+            <button class = "btn btn-secondary" onClick={() => updateSelector(c.name)}>
+              {c.name}
+            </button>
+          ))
+        }
+      </div>
+      
     </div>
   );
 }
